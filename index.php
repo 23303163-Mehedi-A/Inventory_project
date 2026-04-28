ae379b8e8d3d21ece4a748aba0632a7e4954a77e
ArefinRonok <arefinronok13>
2026-04-29 01:03:47 +0600

    initial inventory project setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'db.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inventory System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Inventory & Order Management System</h1>
+
+<p>System is running successfully ✔</p>
+
+</body>
+</html>
